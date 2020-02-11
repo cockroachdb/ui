52e2e022d69c39b06f53f805f661f8cc4ab3c888
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2020-02-11 16:48:44 -0500

    Adding linting
    Adding linting with the ESlint and Prettier (using the same
    configuration as CockroachCloud) and additionally running linting as a
    part of jest using `jest-runner-eslint`.

--- packages/ui-components/jest.config.js
+++ packages/ui-components/jest.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  moduleNameMapper: {
-    "\\.(css|scss)$": "identity-obj-proxy",
-  },
-  setupFilesAfterEnv: ["jest-enzyme"],
-  testEnvironment: "enzyme",
+  projects: ["jest.testing.config.js", "jest.lint.config.js"],
 };
