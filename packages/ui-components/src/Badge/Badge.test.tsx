d7796b8841401b6484fce119a5ff6ecbc26a478b
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2020-02-18 10:31:00 -0500

    Storybook setup for UI Components
    This is a pretty minimal setup on which stories can be build on top of.
    It creates a simple placeholder theme, installs a few addons, and has a
    simple utility for adding sugar to the knobs addon. All to indicate a
    direction that may be used as a pattern for future storybook
    installations.

--- packages/ui-components/src/Badge/Badge.test.tsx
+++ packages/ui-components/src/Badge/Badge.test.tsx
@@ -4,43 +4,9 @@ import { shallow } from "enzyme";
 import Badge from "./Badge";
 
 describe("Badge content", () => {
-<<<<<<< HEAD
-||||||| merged common ancestors
-  test("can render using content prop", () => {
-    const wrapper = renderSubject({ content: "test" });
-    expect(wrapper.prop("children")).toBe("test");
-  });
-
-=======
-  test("can render using content prop", () => {
-    const wrapper = renderSubject({ content: "test" });
-    expect(wrapper).toMatchSnapshot();
-  });
-
->>>>>>> adding snapshots and tokens.scss
   test("can render using children", () => {
     const wrapper = shallow(<Badge>child content</Badge>);
-<<<<<<< HEAD
     expect(wrapper).toMatchSnapshot();
-||||||| merged common ancestors
-    expect(wrapper.prop("children")).toBe("child content");
-  });
-
-  test("rendered by children should override content from the content prop", () => {
-    const wrapper = shallow(
-      <Badge content="prop content">child content</Badge>,
-    );
-    expect(wrapper.prop("children")).toBe("child content");
-=======
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  test("rendered by children should override content from the content prop", () => {
-    const wrapper = shallow(
-      <Badge content="prop content">child content</Badge>,
-    );
-    expect(wrapper).toMatchSnapshot();
->>>>>>> adding snapshots and tokens.scss
   });
 
   test("if empty shouldn't render", () => {
