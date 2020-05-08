55ab4176d53727f7292b81efb119149e69dac8ec
Vlad <carrott9@gmail.com>
2020-05-08 18:55:26 +0300

    typo fix

--- packages/storybook-ui-components/stories/2-Tooltip.stories.js
+++ packages/storybook-ui-components/stories/2-Tooltip.stories.js
@@ -137,7 +137,6 @@ export const Demo = () => (
         "bottom"
       )}
       style={select("Style", ["default", "dark", "light"], "default")}
-      visible={true}
       content={text(
         "Text",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
