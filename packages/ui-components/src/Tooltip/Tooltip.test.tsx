3086691addf00e9c8fda64ad9886423526a75485
Vlad <carrott9@gmail.com>
2020-05-08 18:51:48 +0300

    tooltip component styling, tests and readme

--- packages/ui-components/src/Tooltip/Tooltip.test.tsx
+++ packages/ui-components/src/Tooltip/Tooltip.test.tsx
@@ -1,13 +1,40 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 import Tooltip from "./Tooltip";
 
-describe("Tooltip content", () => {
-  // test("can render", () => {
-  //   const wrapper = shallow(<Badge/>);
-  //   expect(wrapper).toMatchSnapshot();
-  // });
+const TooltipExample = (
+  <Tooltip content={<div id="content">test</div>}>
+    <div id="hoverme">hover me</div>
+  </Tooltip>
+);
 
+describe("Tooltip", () => {
+  test("renders", () => {
+    const wrapper = shallow(TooltipExample);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("is shows children", () => {
+    const wrapper = shallow(TooltipExample);
+    expect(wrapper.find("#hoverme").text()).toBe("hover me");
+  });
+
+  it("is shown on hover", () => {
+    const wrapper = mount(TooltipExample);
+    expect(wrapper.find(".crl-tooltip").length).toBeFalsy();
+    wrapper.find("#hoverme").simulate("mouseEnter");
+    expect(wrapper.find(".crl-tooltip").length).toBeTruthy();
+  });
+
+  it("is not shown on hover if content is empty", () => {
+    const wrapper = mount(
+      <Tooltip content={undefined}>
+        <div id="hoverme">hover me</div>
+      </Tooltip>,
+    );
+    expect(wrapper.find(".crl-tooltip").length).toBeFalsy();
+    wrapper.find("#hoverme").simulate("mouseEnter");
+    expect(wrapper.find(".crl-tooltip").length).toBeFalsy();
+  });
 });
-
