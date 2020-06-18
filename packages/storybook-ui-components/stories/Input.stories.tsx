4c30bacf24d7a593ef0595eb97dd455452c4f94c
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2020-06-18 14:54:42 -0400

    Stubbing html content in a story

--- packages/storybook-ui-components/stories/Input.stories.tsx
+++ packages/storybook-ui-components/stories/Input.stories.tsx
@@ -12,13 +12,37 @@ export default {
 export const Number = () => {
   const [value, setValue] = useState<number | undefined>();
   return (
-    <NumberInput
-      initialValue={number("Initial value", 0)}
-      value={value}
-      disabled={boolean("disabled", false)}
-      onChange={setValue}
-      invalid={boolean("invalid", false)}
-    />
+    <section>
+      {/** 
+       * Note from Nathan to design friends:
+       * 
+       * Everything in these curly brackets are comments. Any text entered in here
+       * won't show up on the page. Inside of this <section> tag you can write any
+       * valid HTML. I would leave the <NumberInput /> alone, but everything else
+       * should be fine.
+       * 
+       * In this context (which is still parsed in React) css can sometimes be tricky,
+       * so for small styling changes you may use local style attributes. Example:
+       * 
+       * <span style={{ color: red; font-weight: bold; }}>This is some bold red content</span
+       * 
+       * Notice that the value of the style attribute is wrapped in {{ ... }} and not quotes. The content
+       * of the "style object" is the same as it would appear in a CSS file. Ping me for questions.
+      */}
+
+      <h1>Example Number Input header</h1>
+
+      <div style={{ margin: "2rem 0" }}>example description</div>
+
+      <NumberInput
+        initialValue={number("Initial value", 0)}
+        value={value}
+        disabled={boolean("disabled", false)}
+        onChange={setValue}
+        invalid={boolean("invalid", false)}
+      />
+
+    </section>
   );
 };
 
