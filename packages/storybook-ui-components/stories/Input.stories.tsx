43067796f7924766264160c0db7ed97ebcb03681
Anne Birzin <51920721+Annebirzin@users.noreply.github.com>
2020-06-22 14:30:47 -0400

    added docs add-on

--- packages/storybook-ui-components/stories/Input.stories.tsx
+++ packages/storybook-ui-components/stories/Input.stories.tsx
@@ -12,27 +12,27 @@ export default {
 export const Number = () => {
   const [value, setValue] = useState<number | undefined>();
   return (
-    <section>
-      {/** 
+    <section style={{ marginLeft: "25px", fontFamily: "Opensans"}}>
+      {/**
        * Note from Nathan to design friends:
-       * 
+       *
        * Everything in these curly brackets are comments. Any text entered in here
        * won't show up on the page. Inside of this <section> tag you can write any
        * valid HTML. I would leave the <NumberInput /> alone, but everything else
        * should be fine.
-       * 
+       *
        * In this context (which is still parsed in React) css can sometimes be tricky,
        * so for small styling changes you may use local style attributes. Example:
-       * 
+       *
        * <span style={{ color: red; font-weight: bold; }}>This is some bold red content</span
-       * 
+       *
        * Notice that the value of the style attribute is wrapped in {{ ... }} and not quotes. The content
        * of the "style object" is the same as it would appear in a CSS file. Ping me for questions.
       */}
 
       <h1>Example Number Input header</h1>
 
-      <div style={{ margin: "2rem 0" }}>example description</div>
+      <div style={{ margin: "2rem 0" }}>Use this input for whole numbers. Use arrow selector for small numbers and type within input for larger numbers.</div>
 
       <NumberInput
         initialValue={number("Initial value", 0)}
