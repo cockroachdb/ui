815666fa804e8173d9116383e079f69e004ae08c
Joe Lowinske <joe@cockroachlabs.com>
2020-07-05 16:35:55 -0500

    ui: implement Button component (#55)

---
+++ packages/storybook-ui-components/stories/Button.stories.tsx
@@ -0,0 +1,85 @@
+import React, { FunctionComponent, ReactNode } from "react";
+import { withKnobs, text, select } from "@storybook/addon-knobs";
+import { Button, ButtonIntent, ButtonSize } from "@cockroachlabs/ui-components";
+
+export default {
+  title: "Button",
+  components: Button,
+  decorators: [withKnobs],
+};
+
+const ButtonDisplaySection: FunctionComponent<{
+  children: ReactNode;
+}> = ({ children }) => (
+  <section style={{ display: "flex", justifyContent: "flex-start" }}>
+    {children}
+  </section>
+);
+const ButtonDisplay: FunctionComponent<{ children: ReactNode }> = ({
+  children,
+}) => (
+  <div style={{ margin: "0rem 1rem", textAlign: "center" }}>{children}</div>
+);
+const Label: FunctionComponent<{ children: string }> = ({ children }) => (
+  <div style={{ marginBottom: "1rem" }}>
+    <code>{children}</code>
+  </div>
+);
+
+export const Example = () => (
+  <section>
+    <h1>Buttons</h1>
+    <section>
+      <h3>Type</h3>
+      <ButtonDisplaySection>
+        <ButtonDisplay>
+          <Label>primary-success</Label>
+          <Button intent="primary-success">Success</Button>
+        </ButtonDisplay>
+        <ButtonDisplay>
+          <Label>primary-danger</Label>
+          <Button intent="primary-danger">Danger</Button>
+        </ButtonDisplay>
+        <ButtonDisplay>
+          <Label>secondary (default)</Label>
+          <Button>Secondary</Button>
+        </ButtonDisplay>
+        <ButtonDisplay>
+          <Label>tertiary</Label>
+          <Button intent="tertiary">Tertiary</Button>
+        </ButtonDisplay>
+      </ButtonDisplaySection>
+    </section>
+    <section>
+      <h3>Size</h3>
+      <ButtonDisplaySection>
+        <ButtonDisplay>
+          <Label>standard (default)</Label>
+          <Button>Standard</Button>
+        </ButtonDisplay>
+        <ButtonDisplay>
+          <Label>small</Label>
+          <Button size="small">Small</Button>
+        </ButtonDisplay>
+      </ButtonDisplaySection>
+    </section>
+  </section>
+);
+
+export const Demo = () => {
+  const intents: Array<ButtonIntent> = [
+    "primary-success",
+    "primary-danger",
+    "secondary",
+    "tertiary",
+  ];
+  const sizes: Array<ButtonSize> = ["standard", "small"];
+  return (
+    <Button
+      intent={select("Intent", intents, "primary-success")}
+      size={select("Size", sizes, "standard")}
+    >
+      {text("Text", "button")}
+    </Button>
+  );
+};
--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -1,5 +1,6 @@
-export * from "./Badge/Badge";
 export * from "./Avatar/Avatar";
+export * from "./Badge/Badge";
+export * from "./Button/Button";
 export * from "./Input/";
 export * from "./Tooltip/Tooltip";
 export * from "./Icon/Icon";
