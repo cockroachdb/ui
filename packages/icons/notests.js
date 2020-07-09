7a21222a73ef3827c942113ceb4fc73e6139a2ce
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2020-07-09 17:01:25 -0400

    Making some adjustments to the packages for less publish friction (#89)
    Test publishing to troubleshoot some issues in with `lerna publish`
    
    * Publish
    
     - @cockroachlabs/design-tokens@0.0.5
     - @cockroachlabs/eslint-config@0.1.7
     - @cockroachlabs/icons@0.2.6
     - storybook-ui-components@0.2.1
     - @cockroachlabs/ui-components@0.2.1
    
    * Publish
    
     - @cockroachlabs/icons@0.2.7
     - storybook-ui-components@0.2.2
     - @cockroachlabs/ui-components@0.2.2
    
    * Publish
    
     - @cockroachlabs/icons@0.2.8
     - storybook-ui-components@0.2.3
     - @cockroachlabs/ui-components@0.2.3
    
    * Publish
    
     - storybook-ui-components@0.2.4
     - @cockroachlabs/ui-components@0.2.4
    
    * Publish
    
     - storybook-ui-components@0.2.5
     - @cockroachlabs/ui-components@0.2.5
    
    * Publish
    
     - @cockroachlabs/icons@0.2.9-alpha.0
     - storybook-ui-components@0.2.6-alpha.0
     - @cockroachlabs/ui-components@0.2.6-alpha.0

--- packages/icons/notests.js
+++ packages/icons/notests.js
@@ -2,9 +2,15 @@ const emoji = require("node-emoji");
 const chalk = require("chalk");
 
 console.log(`
-${emoji.emojify(`:ghost: :ghost: :ghost: ${chalk.bold.red("Nope!")} :ghost: :ghost: :ghost:`)}
+${emoji.emojify(
+  `:ghost: :ghost: :ghost: ${chalk.bold.red("Nope!")} :ghost: :ghost: :ghost:`,
+)}
 
 There are no tests here because this package contains no code.
-It is fully dependent on ${emoji.get("package")} ${chalk.bold("@svgr/webpack")} ${emoji.get("package")}, so you can look at their
-tests at ${chalk.cyanBright("https://github.com/gregberge/svgr/tree/master/packages/webpack")}.
+It is fully dependent on ${emoji.get("package")} ${chalk.bold(
+  "@svgr/cli",
+)} ${emoji.get("package")}, so you can look at their
+tests at ${chalk.cyanBright(
+  "https://github.com/gregberge/svgr/tree/master/packages/cli",
+)}.
 `);
