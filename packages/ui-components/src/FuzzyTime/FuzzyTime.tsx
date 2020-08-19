af3f90e8c9777222cfd39eb9cc811aabbd800191
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2020-08-19 16:00:40 -0400

    Extending props with Native Span props
    Pass through span props to allow implementor to extend FuzzyTime with
    native span props

--- packages/ui-components/src/FuzzyTime/FuzzyTime.tsx
+++ packages/ui-components/src/FuzzyTime/FuzzyTime.tsx
@@ -1,20 +1,30 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, HTMLAttributes } from "react";
 import classnames from "classnames/bind";
 
 import { fuzzy } from "./util";
 import styles from "./FuzzyTime.module.scss";
 
-export interface FuzzyTimeProps {
+type OwnFuzzyProps = {
   timestamp: Date;
-}
+};
+
+export type FuzzyTimeProps = OwnFuzzyProps &
+  React.HTMLAttributes<HTMLSpanElement>;
 
 const cx = classnames.bind(styles);
 
-export const FuzzyTime: FunctionComponent<FuzzyTimeProps> = ({ timestamp }) => {
+export const FuzzyTime: FunctionComponent<FuzzyTimeProps> = ({
+  timestamp,
+  ...rest
+}) => {
   const timeago = fuzzy(timestamp);
   const classnames = cx("fuzzy-time");
 
-  return <span className={classnames}>{timeago}</span>;
+  return (
+    <span className={classnames} {...rest}>
+      {timeago}
+    </span>
+  );
 };
 
 export * from "./util";
