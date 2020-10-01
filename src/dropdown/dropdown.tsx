24c350df1023ec80280c003b91a454f806fec46a
Andrii Vorobiov <and.vorobiov@gmail.com>
2020-10-01 16:26:16 +0300

    rewrite Dropdown component with new designs
    The main reason to rewrite entirely Dropdown component is
    that it was very difficult to maintain, extend to current
    needs. Lots of custom logic was added inside so it felt like
    very custom component.
    
    Current component is designed to easily customize it by
    overriding values via props and predefined options satisfy
    custom designs.

--- src/dropdown/dropdown.tsx
+++ src/dropdown/dropdown.tsx
@@ -1,168 +1,153 @@
-/* eslint-disable @typescript-eslint/camelcase */
 import React from "react";
-import Select from "react-select";
-import classNames from "classnames/bind";
-import _ from "lodash";
+import classnames from "classnames/bind";
 
-import { CaretDown, CaretLeft, CaretRight } from "@cockroachlabs/icons";
-import "react-select/scss/default.scss";
+import { OutsideEventHandler } from "../outsideEventHandler";
 import styles from "./dropdown.module.scss";
+import { Button, ButtonProps } from "src/button";
+import { CaretDown } from "@cockroachlabs/icons";
 
-export interface DropdownOption {
-  value: string;
-  label: string;
+const cx = classnames.bind(styles);
+
+export interface DropdownOption<T = string> {
+  value: T;
+  name: React.ReactNode | string;
 }
 
-export enum ArrowDirection {
-  LEFT,
-  RIGHT,
-  CENTER,
-}
-
-interface DropdownOwnProps {
-  title: string;
-  selected: string;
-  options: DropdownOption[];
-  onChange?: (selected: DropdownOption) => void; // Callback when the value changes.
-  // If onArrowClick exists, don't display the arrow next to the dropdown,
-  // display left and right arrows to either side instead.
-  onArrowClick?: (direction: ArrowDirection) => void;
-  // Disable any arrows in the arrow direction array.
-  disabledArrows?: ArrowDirection[];
-  content?: any;
-  isTimeRange?: boolean;
+export interface DropdownProps<T> {
+  items: Array<DropdownOption<T>>;
+  onChange: (item: DropdownOption<T>["value"]) => void;
+  children?: React.ReactNode;
+  customToggleButton?: React.ReactNode;
+  customToggleButtonOptions?: Partial<ButtonProps>;
+  menuPosition?: "left" | "right";
   className?: string;
-  type?: "primary" | "secondary";
 }
 
-const cx = classNames.bind(styles);
+interface DropdownState {
+  isOpen: boolean;
+}
 
-export const arrowRenderer = ({ isOpen }: { isOpen: boolean }) => (
-  <span className={cx("caret-down", { active: isOpen })}>
-    <CaretDown className={cx("dropdown-caret")} />
-  </span>
-);
+interface DropdownButtonProps {
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+  customProps?: Partial<ButtonProps>;
+}
 
-/**
- * Dropdown component that uses the URL query string for state.
- */
-export class Dropdown extends React.Component<DropdownOwnProps, {}> {
+const DropdownButton: React.FC<DropdownButtonProps> = ({
+  children,
+  customProps = {},
+}) => {
+  return (
+    <Button
+      type="secondary"
+      size="default"
+      iconPosition="right"
+      icon={<CaretDown />}
+      {...customProps}
+    >
+      {children}
+    </Button>
+  );
+};
+
+function DropdownItem<T = string>(props: DropdownItemProps<T>) {
+  const { children, value, onClick } = props;
+  return (
+    <div onClick={() => onClick(value)} className={cx("crl-dropdown__item")}>
+      {children}
+    </div>
+  );
+}
+
+export class Dropdown<T = string> extends React.Component<
+  DropdownProps<T>,
+  DropdownState
+> {
   state = {
-    is_focused: false,
+    isOpen: false,
   };
 
-  dropdownRef: React.RefObject<HTMLDivElement> = React.createRef();
-  titleRef: React.RefObject<HTMLDivElement> = React.createRef();
-  selectRef: React.RefObject<Select> = React.createRef();
+  handleMenuOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
 
-  triggerSelectClick = (e: any) => {
-    const dropdownNode = this.dropdownRef.current as Node;
-    const titleNode = this.titleRef.current as Node;
-    const selectNode = this.selectRef.current;
+  changeMenuState = (nextState: boolean) => {
+    this.setState({
+      isOpen: nextState,
+    });
+  };
 
-    if (
-      e.target.isSameNode(dropdownNode) ||
-      e.target.isSameNode(titleNode) ||
-      e.target.className.indexOf("dropdown__select") > -1
-    ) {
-      // This is a far-less-than-ideal solution to the need to trigger
-      // the react-select dropdown from the entirety of the dropdown area
-      // instead of just the nodes rendered by the component itself
-      // the approach borrows from:
-      // https://github.com/JedWatson/react-select/issues/305#issuecomment-172607534
-      //
-      // a broader discussion on the status of a possible feature addition that
-      // would render this hack moot can be found here:
-      // https://github.com/JedWatson/react-select/issues/1989
-      (selectNode as any).handleMouseDownOnMenu(e);
+  handleItemSelection = (value: T) => {
+    this.props.onChange(value);
+    this.handleMenuOpen();
+  };
+
+  renderDropdownToggleButton = () => {
+    const {
+      children,
+      customToggleButton,
+      customToggleButtonOptions,
+    } = this.props;
+    const { isOpen } = this.state;
+
+    if (customToggleButton) {
+      return customToggleButton;
+    } else {
+      return (
+        <DropdownButton isOpen={isOpen} customProps={customToggleButtonOptions}>
+          {children}
+        </DropdownButton>
+      );
     }
   };
 
-  onFocus = () => this.setState({ is_focused: true });
-
-  onClose = () => this.setState({ is_focused: false });
-
   render() {
-    const {
-      selected,
-      options,
-      onChange,
-      onArrowClick,
-      disabledArrows,
-      content,
-      isTimeRange,
-      type = "secondary",
-    } = this.props;
+    const { items, menuPosition = "left", className } = this.props;
+    const { isOpen } = this.state;
 
-    const className = cx(
-      "dropdown",
-      `dropdown--type-${type}`,
+    const menuStyles = cx(
+      "crl-dropdown__menu",
+      `crl-dropdown__menu--align-${menuPosition}`,
       {
-        _range: isTimeRange,
-        "dropdown--side-arrows": !_.isNil(onArrowClick),
-        dropdown__focused: this.state.is_focused,
+        "crl-dropdown__menu--open": isOpen,
       },
-      this.props.className,
     );
-    const leftClassName = cx("dropdown__side-arrow", {
-      "dropdown__side-arrow--disabled": _.includes(
-        disabledArrows,
-        ArrowDirection.LEFT,
-      ),
-    });
-    const rightClassName = cx("dropdown__side-arrow", {
-      "dropdown__side-arrow--disabled": _.includes(
-        disabledArrows,
-        ArrowDirection.RIGHT,
-      ),
-    });
+
+    const menuItems = items.map((menuItem, idx) => (
+      <DropdownItem<T>
+        value={menuItem.value}
+        onClick={this.handleItemSelection}
+        key={idx}
+      >
+        {menuItem.name}
+      </DropdownItem>
+    ));
 
     return (
-      <div
-        className={className}
-        onClick={this.triggerSelectClick}
-        ref={this.dropdownRef}
-      >
-        {/* TODO (maxlang): consider moving arrows outside the dropdown component */}
-        <span
-          className={leftClassName}
-          onClick={() => this.props.onArrowClick(ArrowDirection.LEFT)}
-        >
-          <CaretLeft className={cx("dropdown-caret")} />
-        </span>
-        <span
-          className={cx({
-            "dropdown__range-title": isTimeRange,
-            dropdown__title: !isTimeRange,
-          })}
-          ref={this.titleRef}
-        >
-          {this.props.title}
-          {this.props.title && !isTimeRange ? ":" : ""}
-        </span>
-        {content ? (
-          content
-        ) : (
-          <Select
-            className={cx("dropdown__select")}
-            arrowRenderer={arrowRenderer}
-            clearable={false}
-            searchable={false}
-            options={options}
-            value={selected}
-            onChange={onChange}
-            onFocus={this.onFocus}
-            onClose={this.onClose}
-            ref={this.selectRef}
-          />
-        )}
-        <span
-          className={rightClassName}
-          onClick={() => this.props.onArrowClick(ArrowDirection.RIGHT)}
-        >
-          <CaretRight className={cx("dropdown-caret")} />
-        </span>
+      <div className={cx("crl-dropdown", className)}>
+        <OutsideEventHandler onOutsideClick={() => this.changeMenuState(false)}>
+          <div
+            className={cx("crl-dropdown__handler")}
+            onClick={this.handleMenuOpen}
+          >
+            {this.renderDropdownToggleButton()}
+          </div>
+          <div className={cx("crl-dropdown__overlay")}>
+            <div className={menuStyles}>
+              <div className={cx("crl-dropdown__container")}>{menuItems}</div>
+            </div>
+          </div>
+        </OutsideEventHandler>
       </div>
     );
   }
+}
+
+export interface DropdownItemProps<T> {
+  children: React.ReactNode;
+  value: T;
+  onClick: (value: T) => void;
 }
