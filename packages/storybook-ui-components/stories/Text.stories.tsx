138c9bc1528da3700826d8501cee1f3892ed60e5
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2020-12-04 17:06:17 -0500

    storybook-ui-components: Adding Stories for Type
    Adding stories to demonstrate the use of Type components. I added Roboto
    Mono with weights 400, 500, and 700 to satisfy the style treatments
    for Code.

---
+++ packages/storybook-ui-components/stories/Text.stories.tsx
@@ -0,0 +1,171 @@
+import React, { FunctionComponent, ReactNode } from "react";
+import { withKnobs, text, select } from "@storybook/addon-knobs";
+import { StoryContainer, StoryDescription, Label } from "../layout";
+import {
+  Text,
+  TextType,
+  Heading,
+  HeadingType,
+  Code,
+  CodeWeight,
+} from "@cockroachlabs/ui-components";
+
+export default {
+  title: "Text",
+  components: Text,
+  decorators: [withKnobs],
+};
+
+const TypeSample: FunctionComponent<{
+  children: ReactNode;
+}> = ({ children }) => (
+  <section
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      marginBottom: "2rem",
+    }}
+  >
+    {children}
+  </section>
+);
+
+const Sample: FunctionComponent<{ children: ReactNode }> = ({ children }) => (
+  <div style={{ margin: "1rem", borderTop: "1px dotted" }}>{children}</div>
+);
+
+const pangram = "The five boxing wizards jump quickly";
+const lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum congue non turpis vitae tristique. Cras in tellus pretium, dignissim eros vel, luctus ante. Etiam ipsum nisl, sollicitudin ut ornare vel, fringilla et nunc. Pellentesque tincidunt facilisis leo, in sollicitudin massa ornare sed. Curabitur at sem gravida, feugiat justo vitae, euismod arcu. Mauris eu dictum dolor. Quisque consequat odio orci, vitae dignissim elit vestibulum nec. Mauris ut urna elit. Etiam et mi non eros aliquet tempus nec ut tellus. Nunc ut fringilla dui. Curabitur imperdiet, velit vel tempus gravida, tellus velit malesuada nibh, ut lacinia quam nisi eu nibh. Proin sed augue quis libero mollis lobortis vitae eu lectus. Sed ultrices id lacus quis finibus. Etiam vestibulum, mauris et vestibulum elementum, urna ex mattis diam, at tristique lorem orci at felis. Sed ultrices tempus libero.";
+const sample =
+  "CREATE TABLE animals (id INT PRIMARY KEY DEFAULT unique_rowid(), name STRING);";
+const headingTypes: Array<HeadingType> = ["h1", "h2", "h3", "h4", "h5", "h6"];
+const textTypes: Array<TextType> = [
+  "body",
+  "body-strong",
+  "caption",
+  "caption-strong",
+  "code",
+];
+const codeTypes: Array<CodeWeight> = ["regular", "medium", "bold"];
+
+export const Example = () => (
+  <StoryContainer>
+    <h1>Text</h1>
+
+    <StoryDescription>
+      Text is a component for rendering type in Cockroach Labs standard
+      treatments
+    </StoryDescription>
+
+    <section>
+      <h3>Heading</h3>
+      <TypeSample>
+        <Sample>
+          <Label>Heading 1</Label>
+          <Heading type="h1">{pangram}</Heading>
+        </Sample>
+
+        <Sample>
+          <Label>Heading 2</Label>
+          <Heading type="h2">{pangram}</Heading>
+        </Sample>
+
+        <Sample>
+          <Label>Heading 3</Label>
+          <Heading type="h3">{pangram}</Heading>
+        </Sample>
+
+        <Sample>
+          <Label>Heading 4</Label>
+          <Heading type="h4">{pangram}</Heading>
+        </Sample>
+
+        <Sample>
+          <Label>Heading 5</Label>
+          <Heading type="h5">{pangram}</Heading>
+        </Sample>
+
+        <Sample>
+          <Label>Heading 6</Label>
+          <Heading type="h6">{pangram}</Heading>
+        </Sample>
+      </TypeSample>
+    </section>
+
+    <section>
+      <h3>Type</h3>
+      <TypeSample>
+        <Sample>
+          <Label>Body</Label>
+          <Text type="body">{lorem}</Text>
+        </Sample>
+
+        <Sample>
+          <Label>Body Strong</Label>
+          <Text type="body-strong">{lorem}</Text>
+        </Sample>
+
+        <Sample>
+          <Label>Caption</Label>
+          <Text type="caption">{lorem}</Text>
+        </Sample>
+
+        <Sample>
+          <Label>Caption Strong</Label>
+          <Text type="caption-strong">{lorem}</Text>
+        </Sample>
+      </TypeSample>
+    </section>
+
+    <section>
+      <TypeSample>
+        <Sample>
+          <Label>Code Regular</Label>
+          <Code weight="regular">{sample}</Code>
+        </Sample>
+
+        <Sample>
+          <Label>Code Regular</Label>
+          <Code weight="medium">{sample}</Code>
+        </Sample>
+
+        <Sample>
+          <Label>Code Regular</Label>
+          <Code weight="bold">{sample}</Code>
+        </Sample>
+      </TypeSample>
+    </section>
+  </StoryContainer>
+);
+
+export const Demo = () => (
+  <StoryContainer>
+    <h1>Demo</h1>
+    <section>
+      <h3>Heading</h3>
+      <div>
+        <Heading type={select("Heading Type", headingTypes, "h1")}>
+          {text("Heading Text", pangram)}
+        </Heading>
+      </div>
+    </section>
+    <section>
+      <h3>Text</h3>
+      <div>
+        <Text type={select("Text Type", textTypes, "body")}>
+          {text("Text", lorem)}
+        </Text>
+      </div>
+    </section>
+    <section>
+      <h3>Code</h3>
+      <div>
+        <Code weight={select("Code Weight", codeTypes, "regular")}>
+          {text("Code Text", sample)}
+        </Code>
+      </div>
+    </section>
+  </StoryContainer>
+);
