ff16cb72c201a00a8bba22bf3f82194a69c342b1
Pooja Maniar <poojam@cockroachlabs.com>
2020-12-08 17:27:11 -0500

    ui: Do not export BaseTextInput

--- packages/ui-components/src/Input/index.ts
+++ packages/ui-components/src/Input/index.ts
@@ -1,4 +1,4 @@
 export * from "./Field";
 export * from "./EmailPasswordInput";
 export * from "./CheckboxInput";
-export * from "./TextTypeInput";
+export { TextInput, TextInputProps, NumberInput, NumberProps } from "./TextTypeInput";
