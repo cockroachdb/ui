6ab01105b29f11893addd63e1e088ad082c11587
Vlad Los <carrott9@gmail.com>
2021-02-09 15:59:32 +0200

    cluster-ui: sessions redux functionality
    all functions except alerts about success\error on query termination.

--- packages/cluster-ui/src/sessions/sessionsPage.tsx
+++ packages/cluster-ui/src/sessions/sessionsPage.tsx
@@ -39,17 +39,18 @@ import TerminateSessionModal, {
   TerminateSessionModalRef,
 } from "./terminateSessionModal";
 
-import sortableTableStyles from "src/sortedtable/sortedtable.module.scss";
-
-const sortableTableCx = classNames.bind(sortableTableStyles);
-
-type ICancelQueryRequest = any;
+import sortedTableStyles from "src/sortedtable/sortedtable.module.scss";
+import {
+  CancelSessionRequestMessage,
+  CancelQueryRequestMessage,
+} from "src/api/terminateQueryApi";
+const sortableTableCx = classNames.bind(sortedTableStyles);
 interface OwnProps {
   sessions: SessionInfo[];
-  sessionsError: Error | null;
-  //refreshSessions: typeof refreshSessions;
-  refreshSessions: any;
-  cancel?: (req: ICancelQueryRequest) => void;
+  sessionsError: Error | Error[];
+  refreshSessions: () => void;
+  cancelSession: (req: CancelSessionRequestMessage) => void;
+  cancelQuery: (req: CancelQueryRequestMessage) => void;
   onPageChanged?: (newPage: number) => void;
 }
 
@@ -154,6 +155,7 @@ export class SessionsPage extends React.Component<
   renderSessions = () => {
     const sessionsData = this.props.sessions;
     const { pagination } = this.state;
+
     return (
       <div>
         <section className={sortableTableCx("cl-table-container")}>
@@ -204,7 +206,7 @@ export class SessionsPage extends React.Component<
   };
 
   render() {
-    const { match, cancel } = this.props;
+    const { match, cancelSession, cancelQuery } = this.props;
     const app = getMatchParamByName(match, appAttr);
     return (
       <React.Fragment>
@@ -219,8 +221,14 @@ export class SessionsPage extends React.Component<
           error={this.props.sessionsError}
           render={this.renderSessions}
         />
-        <TerminateSessionModal ref={this.terminateSessionRef} cancel={cancel} />
-        <TerminateQueryModal ref={this.terminateQueryRef} cancel={cancel} />
+        <TerminateSessionModal
+          ref={this.terminateSessionRef}
+          cancel={cancelSession}
+        />
+        <TerminateQueryModal
+          ref={this.terminateQueryRef}
+          cancel={cancelQuery}
+        />
       </React.Fragment>
     );
   }
