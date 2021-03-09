4f1fa9ce3c2ca84c08e403bb070987f8cb94804f
Tharun Rajendran <rajendrantharun@live.com>
2021-03-09 22:57:01 +0530

    Refractor avatar unit tests to use react testing library (#227)
    * Refractor avatar unit tests to use react testing library
    
    * fix lint command and add update jest config
    
    * refractor avatar tests based on updated jest config
    
    * remove data-testid from avatar component
    
    Signed-off-by: Tharun <rajendrantharun@live.com>

--- packages/ui-components/src/Avatar/Avatar.test.tsx
+++ packages/ui-components/src/Avatar/Avatar.test.tsx
@@ -1,75 +1,97 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render, fireEvent, screen } from "@testing-library/react";
 
-import Avatar, { AvatarIntent, AvatarSize } from "./Avatar";
+import UiAvatar, { AvatarIntent, AvatarSize, AvatarProps } from "./Avatar";
+
+const Avatar: React.FC<AvatarProps> = ({ children, ...props }) => (
+  <UiAvatar data-testid="avatar" {...props}>
+    {children}
+  </UiAvatar>
+);
 
 describe("Avatar", () => {
   describe("Default props", () => {
     it("assigns default style classes based on props", () => {
-      const wrapper = shallow(<Avatar>CL</Avatar>);
-      const className = wrapper.prop("className");
-      expect(className).toContain("intent-default");
-      expect(className).toContain("size-default");
-      expect(className).toContain("transformCase-uppercase");
-      expect(className).not.toContain("disabled");
-      expect(className).not.toContain("selectable");
+      render(<Avatar>CL</Avatar>);
+      expect(screen.getByTestId("avatar")).toHaveClass(
+        "intent-default",
+        "size-default",
+        "transformCase-uppercase",
+      );
+      expect(screen.getByTestId("avatar")).not.toHaveClass(
+        "disabled",
+        "selectable",
+      );
     });
   });
 
   describe("Native element props", () => {
     it("appends user provided className", () => {
       const testClass = "nathan-test-stuff";
-      const wrapper = shallow(<Avatar className={testClass}>NS</Avatar>);
-      const className = wrapper.prop("className");
-      expect(className).toContain(testClass);
-      expect(className).toContain("intent-default");
+      render(<Avatar className={testClass}>NS</Avatar>);
+      expect(screen.getByTestId("avatar")).toHaveClass(
+        testClass,
+        "intent-default",
+      );
     });
 
     it("accepts data attributes", () => {
       const datatest = "nathan-data";
-      const wrapper = shallow(<Avatar data-test={datatest}>NS</Avatar>);
-      const result = wrapper.prop("data-test");
-      expect(result).toBe(datatest);
+      render(<Avatar data-test={datatest}>NS</Avatar>);
+      expect(screen.getByTestId("avatar")).toHaveAttribute(
+        "data-test",
+        datatest,
+      );
     });
   });
 
   describe("Handle onClick prop", () => {
     it("calls callback function on element click", () => {
-      const onClickSpy = jasmine.createSpy();
-      const wrapper = shallow(<Avatar onClick={onClickSpy}>CL</Avatar>);
-      wrapper.simulate("click");
+      const onClickSpy = jest.fn();
+      render(<Avatar onClick={onClickSpy}>CL</Avatar>);
+      fireEvent.click(screen.getByTestId("avatar"));
       expect(onClickSpy).toHaveBeenCalled();
     });
 
     it("does not call onClick function when disabled prop is True", () => {
-      const onClickSpy = jasmine.createSpy();
-      const wrapper = shallow(
+      const onClickSpy = jest.fn();
+      render(
         <Avatar disabled={true} onClick={onClickSpy}>
           CL
         </Avatar>,
       );
-      wrapper.simulate("click");
+      fireEvent.click(screen.getByTestId("avatar"));
       expect(onClickSpy).not.toHaveBeenCalled();
     });
   });
 
   describe("Content rendering", () => {
     it("renders with string passed as children", () => {
-      expect(shallow(<Avatar>foo bar</Avatar>).text()).toEqual("foo bar");
+      render(<Avatar>foo bar</Avatar>);
+      expect(screen.getByTestId("avatar")).toHaveTextContent("foo bar");
     });
 
     it("renders with empty content", () => {
-      expect(shallow(<Avatar />).text()).toEqual("");
+      render(<Avatar />);
+      expect(screen.getByTestId("avatar")).toHaveTextContent("");
     });
 
     it("renders with svg passed as children", () => {
       const svg = <svg />;
-      expect(shallow(<Avatar>{svg}</Avatar>).find("svg")).toHaveLength(1);
+      expect(
+        render(<Avatar>{svg}</Avatar>)
+          .getByTestId("avatar")
+          .getElementsByTagName("svg"),
+      ).toHaveLength(1);
     });
 
     it("renders with svg passed as children", () => {
       const img = <img />;
-      expect(shallow(<Avatar>{img}</Avatar>).find("img")).toHaveLength(1);
+      expect(
+        render(<Avatar>{img}</Avatar>)
+          .getByTestId("avatar")
+          .getElementsByTagName("img"),
+      ).toHaveLength(1);
     });
   });
 
@@ -79,10 +101,8 @@ describe("Avatar", () => {
     intents.forEach(intent => {
       it("applies correct classNames depending on intent", () => {
         expect(
-          shallow(<Avatar intent={intent} />)
-            .find("div.avatar")
-            .hasClass(`intent-${intent}`),
-        ).toBeTruthy();
+          render(<Avatar intent={intent} />).getByTestId("avatar"),
+        ).toHaveClass(`intent-${intent}`);
       });
     });
   });
@@ -93,25 +113,23 @@ describe("Avatar", () => {
     sizes.forEach(size => {
       it("applies correct classNames depending on size", () => {
         expect(
-          shallow(<Avatar size={size} />)
-            .find("div.avatar")
-            .hasClass(`size-${size}`),
-        ).toBeTruthy();
+          render(<Avatar size={size} />).getByTestId("avatar"),
+        ).toHaveClass(`size-${size}`);
       });
     });
   });
 
   describe("Disabled prop", () => {
     it("excludes intent className", () => {
-      const wrapper = shallow(<Avatar intent="invalid" disabled />);
-      expect(wrapper.hasClass("disabled")).toBeTruthy();
-      expect(wrapper.hasClass("intent-invalid")).toBeFalsy();
+      render(<Avatar intent="invalid" disabled />);
+      expect(screen.getByTestId("avatar")).toHaveClass("disabled");
+      expect(screen.getByTestId("avatar")).not.toHaveClass("intent-invalid");
     });
 
     it("excludes selectable className", () => {
-      const wrapper = shallow(<Avatar disabled selectable />);
-      expect(wrapper.hasClass("disabled")).toBeTruthy();
-      expect(wrapper.hasClass("selectable")).toBeFalsy();
+      render(<Avatar disabled selectable />);
+      expect(screen.getByTestId("avatar")).toHaveClass("disabled");
+      expect(screen.getByTestId("avatar")).not.toHaveClass("selectable");
     });
   });
 });
