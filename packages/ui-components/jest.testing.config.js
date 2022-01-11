65f92759b861b0221e07cb076bdc9d96b62bb999
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2022-01-11 13:04:45 -0500

    WIP: Attempting to test Color in React Testing Library
    I thought that I could simply compare the color of the rendered
    component to the expected color of our design tokens. The issue
    that I'm currently stuck on is that Jest won't transpile any
    dependencies (meaning that if I import the existing tokens Jest will
    fail, not recognizing `export`). If I compile the tokens to a CommonJS
    module then TypeScript will fail the test saying there are no type
    definitions.
    
    Beyond that, is this even a strategy that we like? How else could be
    replace a test for appropriate intent?

--- packages/ui-components/jest.testing.config.js
+++ packages/ui-components/jest.testing.config.js
@@ -5,9 +5,7 @@ module.exports = {
     "\\.(css|scss)$": "identity-obj-proxy",
   },
   roots: ["<rootDir>/src"],
-  setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect",
-  ],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   testEnvironment: "jsdom",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   transform: {
--- packages/ui-components/src/InlineAlert/InlineAlert.test.tsx
+++ packages/ui-components/src/InlineAlert/InlineAlert.test.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import chromatism from "chromatism";
 import { render, screen } from "@testing-library/react";
-import { InlineAlert } from "./InlineAlert";
+import {
+  ComponentInlineAlertBackgroundColorInfo,
+  ComponentInlineAlertBackgroundColorDanger,
+  ComponentInlineAlertBackgroundColorWarning,
+  ComponentInlineAlertBackgroundColorSuccess,
+} from "@cockroachlabs/design-tokens/dist/web/tokens.module";
+import { InlineAlert, InlineAlertIntent } from "./InlineAlert";
 
 describe("InlineAlert", () => {
+  test("renders with correct intent", () => {
+    const title = "Hello world!";
+    const testClassName = "test-classname";
+    const alertIntents: Array<[InlineAlertIntent, string]> = [
+      ["warning", ComponentInlineAlertBackgroundColorWarning],
+      ["success", ComponentInlineAlertBackgroundColorSuccess],
+      ["danger", ComponentInlineAlertBackgroundColorDanger],
+      ["info", ComponentInlineAlertBackgroundColorInfo],
+    ];
+    alertIntents.forEach(([intent, color]) => {
+      const { container } = render(
+        <InlineAlert title={title} intent={intent} className={testClassName} />,
+      );
+      const elem = container.getElementsByClassName(testClassName);
+      const style = window.getComputedStyle(elem[0]);
+      expect(chromatism.convert(style.backgroundColor).hex).toBe(color);
+    });
+  });
+
   test("renders with provided title", () => {
     const title = "Hello world!";
     render(<InlineAlert title={title} />);
