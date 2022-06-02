ca6f4512d09be270f5b9c5139608abcd85467082
Josephine Lee <josephine@cockroachlabs.com>
2022-06-02 11:37:20 -0400

    Export the ExpandableText component
    This commit exports the ExpandableText component in the ui-components package.

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -6,6 +6,7 @@ export * from "./Tooltip/Tooltip";
 export * from "./Icon/";
 export * from "./FuzzyTime/FuzzyTime";
 export * from "./InlineAlert/InlineAlert";
+export * from "./ExpandableText/ExpandableText";
 export * from "./Spinner/Spinner";
 export * from "./Typography/";
 export * from "./Logo/";
