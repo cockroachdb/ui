f8156b8bff2dee832a69d55733069a9ee521e32d
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2022-10-10 13:41:23 -0400

    Removing dependencies from no test message
    I dropped the formatting dependencies from notest.js in the icons
    package as they were causing an error about dynamic importing.

--- packages/icons/notests.js
+++ packages/icons/notests.js
@@ -1,16 +1,5 @@
-const emoji = require("node-emoji");
-const chalk = require("chalk");
-
 console.log(`
-${emoji.emojify(
-  `:ghost: :ghost: :ghost: ${chalk.bold.red("Nope!")} :ghost: :ghost: :ghost:`,
-)}
-
 There are no tests here because this package contains no code.
-It is fully dependent on ${emoji.get("package")} ${chalk.bold(
-  "@svgr/cli",
-)} ${emoji.get("package")}, so you can look at their
-tests at ${chalk.cyanBright(
-  "https://github.com/gregberge/svgr/tree/master/packages/cli",
-)}.
+It is fully dependent on @svgr/cli. Please refer to their
+tests at https://github.com/gregberge/svgr/tree/master/packages/cli.
 `);
