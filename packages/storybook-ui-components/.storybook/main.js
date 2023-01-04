bcadde2cdd1af8ff69090d0d9eb2cebc52663bf4
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2023-01-04 13:44:32 -0500

    storybook: migrating config files
    Making some changes to the storybook config files to remove some
    version 5.x configs we had (currently on 6.5.x). I removed all of the
    addons for now as there seem to be updated versions of "essential
    addons".

--- packages/storybook-ui-components/.storybook/main.js
+++ packages/storybook-ui-components/.storybook/main.js
@@ -2,19 +2,7 @@ module.exports = {
   core: {
     builder: "webpack5",
   },
-  stories: ["../stories/**/*.stories.(tsx|mdx)"],
+  stories: ["../stories/**/*.stories.@(tsx|mdx)"],
   addons: [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-knobs/register",
-    "@storybook/addon-docs",
-  ],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve("ts-loader"),
-    });
-    config.resolve.extensions.push(".ts", ".tsx");
-    return config;
-  },
+  ]
 };
