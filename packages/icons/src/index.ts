441e28f5ebf7c75ed22a0e1843f5a4208998e26f
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2023-03-03 15:17:07 -0500

    icons, ui-components, storybook: Icon update (#504)

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -12,44 +12,59 @@ o888o o888o o888o o888o  888bod8P' `Y8bod8P' d888b      "888" 8""888P'
 
 // System Icons
 import { default as ArrowLeft } from "./components/ArrowLeft";
+import { default as ArrowRight } from "./components/ArrowRight";
+import { default as ArrowUp } from "./components/ArrowUp";
+import { default as ArrowDown } from "./components/ArrowDown";
 import { default as Backup } from "./components/Backup";
 import { default as Bell } from "./components/Bell";
-import { default as CancelCircleFilled } from "./components/CancelCircleFilled";
-import { default as CancelCircle } from "./components/CancelCircle";
+import { default as Billing } from "./components/Billing";
+import { default as Calendar } from "./components/Calendar";
 import { default as Cancel } from "./components/Cancel";
+import { default as CancelCircle } from "./components/CancelCircle";
+import { default as CancelCircleFilled } from "./components/CancelCircleFilled";
 import { default as CaretDown } from "./components/CaretDown";
 import { default as CaretFilledDown } from "./components/CaretFilledDown";
-import { default as CaretFilledRight } from "./components/CaretFilledRight";
 import { default as CaretLeft } from "./components/CaretLeft";
 import { default as CaretRight } from "./components/CaretRight";
+import { default as CaretFilledRight } from "./components/CaretFilledRight";
 import { default as CaretUp } from "./components/CaretUp";
 import { default as Caution } from "./components/Caution";
-import { default as CheckCircleFilled } from "./components/CheckCircleFilled";
-import { default as CheckCircle } from "./components/CheckCircle";
+import { default as CautionFilled } from "./components/CautionFilled";
 import { default as Check } from "./components/Check";
-import { default as HelpCircleFilled } from "./components/HelpCircleFilled";
+import { default as CheckCircle } from "./components/CheckCircle";
+import { default as CheckCircleFilled } from "./components/CheckCircleFilled";
+import { default as CircleFilled } from "./components/CircleFilled";
+import { default as Collapse } from "./components/Collapse";
 import { default as Copy } from "./components/Copy";
 import { default as Download } from "./components/Download";
-import { default as EllipsisVertical } from "./components/EllipsisVertical";
 import { default as Ellipsis } from "./components/Ellipsis";
+import { default as EllipsisVertical } from "./components/EllipsisVertical";
+import { default as Envelope } from "./components/Envelope";
 import { default as ErrorCircleFilled } from "./components/ErrorCircleFilled";
 import { default as ErrorCircle } from "./components/ErrorCircle";
+import { default as Expand } from "./components/Expand";
 import { default as EyeOff } from "./components/EyeOff";
 import { default as Eye } from "./components/Eye";
 import { default as Gear } from "./components/Gear";
-import { default as InfoCircleFilled } from "./components/InfoCircleFilled";
+import { default as GearFilled } from "./components/GearFilled";
+import { default as FeatureFlags } from "./components/FeatureFlags";
+import { default as HelpCircle } from "./components/HelpCircle";
+import { default as HelpCircleFilled } from "./components/HelpCircleFilled";
 import { default as InfoCircle } from "./components/InfoCircle";
+import { default as InfoCircleFilled } from "./components/InfoCircleFilled";
 import { default as Invalid } from "./components/Invalid";
 import { default as List } from "./components/List";
-import { default as LockFilled } from "./components/LockFilled";
 import { default as Lock } from "./components/Lock";
+import { default as LockFilled } from "./components/LockFilled";
 import { default as MinusCircle } from "./components/MinusCircle";
 import { default as Minus } from "./components/Minus";
 import { default as Open } from "./components/Open";
 import { default as Org } from "./components/Org";
 import { default as Pencil } from "./components/Pencil";
-import { default as PlusCircle } from "./components/PlusCircle";
 import { default as Plus } from "./components/Plus";
+import { default as PlusCircle } from "./components/PlusCircle";
+import { default as PlusCircleFilled } from "./components/PlusCircleFilled";
+import { default as Refresh } from "./components/Refresh";
 import { default as Search } from "./components/Search";
 import { default as Stack } from "./components/Stack";
 import { default as Star } from "./components/Star";
@@ -57,7 +72,6 @@ import { default as Terminal } from "./components/Terminal";
 import { default as Time } from "./components/Time";
 import { default as User } from "./components/User";
 import { default as World } from "./components/World";
-import { default as Upload } from "./components/Upload";
 
 // Pictograms
 import { default as Add } from "./components/Add";
@@ -76,8 +90,15 @@ import { default as SleepyMoon } from "./components/SleepyMoon";
 
 // 3rd Party Icons
 import { default as Aws } from "./components/Aws";
+import { default as Azure } from "./components/Azure";
 import { default as Datadog } from "./components/Datadog";
 import { default as Gcp } from "./components/Gcp";
+import { default as Github } from "./components/Github";
+import { default as Google } from "./components/Google";
+import { default as K3D } from "./components/K3D";
+import { default as Microsoft } from "./components/Microsoft";
+import { default as Slack } from "./components/Slack";
+import { default as Zendesk } from "./components/Zendesk";
 
 // Credit Cards
 import { default as CreditCard } from "./components/CreditCard";
@@ -93,112 +114,103 @@ import { default as Unionpay } from "./components/Unionpay";
 import { default as Australia } from "./components/Australia";
 import { default as Belgium } from "./components/Belgium";
 import { default as Brazil } from "./components/Brazil";
+import { default as Bahrain } from "./components/Bahrain";
 import { default as Canada } from "./components/Canada";
 import { default as Finland } from "./components/Finland";
 import { default as France } from "./components/France";
 import { default as Germany } from "./components/Germany";
 import { default as HongKong } from "./components/Hongkong";
 import { default as India } from "./components/India";
+import { default as Indonesia } from "./components/Indonesia";
 import { default as Ireland } from "./components/Ireland";
 import { default as Japan } from "./components/Japan";
 import { default as Korea } from "./components/Korea";
 import { default as Netherlands } from "./components/Netherlands";
+import { default as Poland } from "./components/Poland";
 import { default as Singapore } from "./components/Singapore";
+import { default as SouthAfrica } from "./components/SouthAfrica";
 import { default as Sweden } from "./components/Sweden";
 import { default as Switzerland } from "./components/Switzerland";
 import { default as Taiwan } from "./components/Taiwan";
 import { default as UnitedKingdom } from "./components/UnitedKingdom";
 import { default as Usa } from "./components/Usa";
 
-// logos
-import { default as CockroachLabsDarkFull } from "./components/CockroachLabsDarkFull";
-import { default as CockroachLabsDarkReduced } from "./components/CockroachLabsDarkReduced";
-import { default as CockroachLabsDarkMono } from "./components/CockroachLabsDarkMono";
-import { default as CockroachLabsLightFull } from "./components/CockroachLabsLightFull";
-import { default as CockroachLabsLightReduced } from "./components/CockroachLabsLightReduced";
-import { default as CockroachLabsLightMono } from "./components/CockroachLabsLightMono";
-import { default as CockroachcloudDarkFull } from "./components/CockroachcloudDarkFull";
-import { default as CockroachcloudDarkReduced } from "./components/CockroachcloudDarkReduced";
-import { default as CockroachcloudDarkMono } from "./components/CockroachcloudDarkMono";
-import { default as CockroachcloudLightFull } from "./components/CockroachcloudLightFull";
-import { default as CockroachcloudLightReduced } from "./components/CockroachcloudLightReduced";
-import { default as CockroachcloudLightMono } from "./components/CockroachcloudLightMono";
-import { default as CockroachdbDarkFull } from "./components/CockroachdbDarkFull";
-import { default as CockroachdbDarkReduced } from "./components/CockroachdbDarkReduced";
-import { default as CockroachdbDarkMono } from "./components/CockroachdbDarkMono";
-import { default as CockroachdbLightFull } from "./components/CockroachdbLightFull";
-import { default as CockroachdbLightReduced } from "./components/CockroachdbLightReduced";
-import { default as CockroachdbLightMono } from "./components/CockroachdbLightMono";
-import { default as CockroachMarkDarkFull } from "./components/CockroachMarkDarkFull";
-import { default as CockroachMarkDarkReduced } from "./components/CockroachMarkDarkReduced";
-import { default as CockroachMarkDarkMono } from "./components/CockroachMarkDarkMono";
-import { default as CockroachMarkLightFull } from "./components/CockroachMarkLightFull";
-import { default as CockroachMarkLightReduced } from "./components/CockroachMarkLightReduced";
-import { default as CockroachMarkLightMono } from "./components/CockroachMarkLightMono";
-import { default as CockroachShorthandBlack } from "./components/CockroachShorthandBlack";
-import { default as CockroachShorthandPurple } from "./components/CockroachShorthandPurple";
-import { default as CockroachShorthandWhite } from "./components/CockroachShorthandWhite";
-
 // other
 import { default as Community } from "./components/Community";
 import { default as Docs } from "./components/Docs";
 import { default as Learning } from "./components/Learning";
+import { default as Upload } from "./components/Upload";
 
-/** 
-oooooooooooo                                               .            
-`888'     `8                                             .o8            
- 888         oooo    ooo oo.ooooo.   .ooooo.  oooo d8b .o888oo  .oooo.o 
- 888oooo8     `88b..8P'   888' `88b d88' `88b `888""8P   888   d88(  "8 
- 888    "       Y888'     888   888 888   888  888       888   `"Y88b.  
- 888       o  .o8"'88b    888   888 888   888  888       888 . o.  )88b 
-o888ooooood8 o88'   888o  888bod8P' `Y8bod8P' d888b      "888" 8""888P' 
-                          888                                           
-                         o888o                                          
+/**
+oooooooooooo                                               .
+`888'     `8                                             .o8
+ 888         oooo    ooo oo.ooooo.   .ooooo.  oooo d8b .o888oo  .oooo.o
+ 888oooo8     `88b..8P'   888' `88b d88' `88b `888""8P   888   d88(  "8
+ 888    "       Y888'     888   888 888   888  888       888   `"Y88b.
+ 888       o  .o8"'88b    888   888 888   888  888       888 . o.  )88b
+o888ooooood8 o88'   888o  888bod8P' `Y8bod8P' d888b      "888" 8""888P'
+                          888
+                         o888o
 */
 
 const SystemIcons = {
   ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+  ArrowDown,
   Backup,
   Bell,
+  Billing,
+  Calendar,
   CancelCircleFilled,
   CancelCircle,
   Cancel,
   CaretDown,
-  CaretFilledDown,
-  CaretFilledRight,
+  CaretFilledDown,         // deprecated
+  CaretFilledRight,        // deprecated
   CaretLeft,
   CaretRight,
   CaretUp,
   Caution,
-  CheckCircleFilled,
-  CheckCircle,
+  CautionFilled,
   Check,
-  HelpCircleFilled,
+  CheckCircle,
+  CheckCircleFilled,
+  CircleFilled,
+  Collapse,
   Copy,
   Download,
-  EllipsisVertical,
   Ellipsis,
+  EllipsisVertical,
+  Envelope,
   ErrorCircleFilled,
   ErrorCircle,
+  Expand,
   EyeOff,
   Eye,
   Gear,
-  InfoCircleFilled,
+  GearFilled,
+  FeatureFlags,
+  HelpCircle,
+  HelpCircleFilled,
   InfoCircle,
+  InfoCircleFilled,
   Invalid,
   List,
   LockFilled,
   Lock,
   MinusCircle,
-  Minus,
+  Minus,                   //deprecated
   Open,
   Org,
   Pencil,
-  PlusCircle,
   Plus,
+  PlusCircle,
+  PlusCircleFilled,
+  Refresh,
   Search,
   Stack,
-  Star,
+  Star,                    //deprecated
   Terminal,
   Time,
   User,
@@ -215,7 +227,18 @@ const Illustrations = {
   SleepyMoon,
 };
 
-const ThirdParty = { Aws, Datadog, Gcp };
+const ThirdParty = {
+  Aws,
+  Azure,
+  Datadog,
+  Gcp,
+  Google,
+  Github,
+  K3D,
+  Microsoft,
+  Slack,
+  Zendesk,
+};
 
 const Cards = {
   CreditCard,
@@ -230,6 +253,7 @@ const Cards = {
 
 const Flags = {
   Australia,
+  Bahrain,
   Belgium,
   Brazil,
   Canada,
@@ -238,11 +262,14 @@ const Flags = {
   Germany,
   HongKong,
   India,
+  Indonesia,
   Ireland,
   Japan,
   Korea,
   Netherlands,
+  Poland,
   Singapore,
+  SouthAfrica,
   Sweden,
   Switzerland,
   Taiwan,
@@ -250,41 +277,16 @@ const Flags = {
   Usa,
 };
 
-const Logos = {
-  CockroachLabsDarkFull,
-  CockroachLabsDarkReduced,
-  CockroachLabsDarkMono,
-  CockroachLabsLightFull,
-  CockroachLabsLightReduced,
-  CockroachLabsLightMono,
-  CockroachcloudDarkFull,
-  CockroachcloudDarkReduced,
-  CockroachcloudDarkMono,
-  CockroachcloudLightFull,
-  CockroachcloudLightReduced,
-  CockroachcloudLightMono,
-  CockroachdbDarkFull,
-  CockroachdbDarkReduced,
-  CockroachdbDarkMono,
-  CockroachdbLightFull,
-  CockroachdbLightReduced,
-  CockroachdbLightMono,
-  CockroachMarkDarkFull,
-  CockroachMarkDarkReduced,
-  CockroachMarkDarkMono,
-  CockroachMarkLightFull,
-  CockroachMarkLightReduced,
-  CockroachMarkLightMono,
-  CockroachShorthandBlack,
-  CockroachShorthandPurple,
-  CockroachShorthandWhite,
-};
-
 export {
   // system icons
   ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+  ArrowDown,
   Backup,
   Bell,
+  Billing,
+  Calendar,
   CancelCircleFilled,
   CancelCircle,
   Cancel,
@@ -295,21 +297,29 @@ export {
   CaretRight,
   CaretUp,
   Caution,
-  CheckCircleFilled,
-  CheckCircle,
+  CautionFilled,
   Check,
-  HelpCircleFilled,
+  CheckCircle,
+  CheckCircleFilled,
+  CircleFilled,
+  Collapse,
   Copy,
   Download,
-  EllipsisVertical,
   Ellipsis,
+  EllipsisVertical,
+  Envelope,
   ErrorCircleFilled,
   ErrorCircle,
+  Expand,
   EyeOff,
   Eye,
   Gear,
-  InfoCircleFilled,
+  GearFilled,
+  FeatureFlags,
+  HelpCircle,
+  HelpCircleFilled,
   InfoCircle,
+  InfoCircleFilled,
   Invalid,
   List,
   LockFilled,
@@ -319,8 +329,10 @@ export {
   Open,
   Org,
   Pencil,
-  PlusCircle,
   Plus,
+  PlusCircle,
+  PlusCircleFilled,
+  Refresh,
   Search,
   Stack,
   Star,
@@ -342,7 +354,15 @@ export {
   SleepyMoon,
   // 3rd party
   Aws,
+  Azure,
+  Datadog,
   Gcp,
+  Google,
+  Github,
+  K3D,
+  Microsoft,
+  Slack,
+  Zendesk,
   // credit cards
   CreditCard,
   Visa,
@@ -372,31 +392,6 @@ export {
   Taiwan,
   UnitedKingdom,
   Usa,
-  // logos
-  CockroachLabsDarkFull,
-  CockroachLabsDarkReduced,
-  CockroachLabsDarkMono,
-  CockroachLabsLightFull,
-  CockroachLabsLightReduced,
-  CockroachLabsLightMono,
-  CockroachcloudDarkFull,
-  CockroachcloudDarkReduced,
-  CockroachcloudDarkMono,
-  CockroachcloudLightFull,
-  CockroachcloudLightReduced,
-  CockroachcloudLightMono,
-  CockroachdbDarkFull,
-  CockroachdbDarkReduced,
-  CockroachdbDarkMono,
-  CockroachdbLightFull,
-  CockroachdbLightReduced,
-  CockroachdbLightMono,
-  CockroachMarkDarkFull,
-  CockroachMarkDarkReduced,
-  CockroachMarkDarkMono,
-  CockroachMarkLightFull,
-  CockroachMarkLightReduced,
-  CockroachMarkLightMono,
   // sets,
   SystemIcons,
   Pictograms,
@@ -404,7 +399,6 @@ export {
   Cards,
   Flags,
   Illustrations,
-  Logos,
   // other
   Community,
   Docs,
