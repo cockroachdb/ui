d4bdaf4e2fa920d49f678c7351ef00c0b4d2f0be
Sean Barag <barag@cockroachlabs.com>
2023-03-24 15:39:37 -0700

    style buttons in nav better

--- packages/ui-components-mui/src/nav/NavItem.tsx
+++ packages/ui-components-mui/src/nav/NavItem.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import Button from "@mui/material/Button";
+import Button, { type ButtonProps } from "@mui/material/Button";
+import { alpha, styled } from "@mui/material/styles";
 
 export interface NavItemProps {
   label: string;
   onClick?: React.DOMAttributes<HTMLElement>["onClick"];
 }
+
+const FocusableNavItem = styled(Button)<ButtonProps>(({ theme }) => ({
+  textTransform: "none",
+  color: alpha("#fff", 0.8),
+  "&:hover, &.Mui-focusVisible": {
+    backgroundColor: alpha("#fff", 0.3),
+  }
+}));
+
 export default function NavItem(props: NavItemProps) {
-    return <Button onClick={props.onClick} sx={{color: "#fff" }}>{props.label}</Button>;
+  return <FocusableNavItem onClick={props.onClick}>{props.label}</FocusableNavItem>;
 }
--- packages/ui-components-mui/src/theme.tsx
+++ packages/ui-components-mui/src/theme.tsx
@@ -1,18 +1,33 @@
 import React, { useMemo } from "react";
 import { createTheme, ThemeProvider as MuiThemeProvider} from "@mui/material/styles";
-import { useMediaQuery } from "@mui/material";
+import { type PaletteOptions, useMediaQuery } from "@mui/material";
 
 const crlTheme = createTheme({ 
   components: {
     MuiButtonBase: {
       defaultProps: {
-        // Disable the Material Design ripple animation globally
+        // Disable the Material Design ripple animation globally.
         disableRipple: true,
-      }
-    }
-  }
-
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          // The header is *always* black.
+          backgroundColor: "#000",
+        },
+      },
+    },
+  },
 });
+
+const lightModePalette: PaletteOptions = {
+  mode: "light",
+};
+
+const darkModePalette: PaletteOptions = {
+  mode: "dark",
+}
 
 interface ThemeProviderProps {
   children?: React.ReactNode;
@@ -20,9 +35,7 @@ interface ThemeProviderProps {
 export default function ThemeProvider(props: ThemeProviderProps) {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   const theme = useMemo(() => createTheme(crlTheme, {
-    palette: { 
-      mode: prefersDarkMode ? "dark" : "light"
-    }
+    palette: prefersDarkMode ? darkModePalette : lightModePalette,
   }), [ prefersDarkMode ]);
   return <MuiThemeProvider theme={theme}>
     {props.children}
