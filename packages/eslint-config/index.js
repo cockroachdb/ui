96f15aad5cfc96d2d2402dd7145268ed75d8b08f
lassenordahl <lasseanordahl@gmail.com>
2023-04-03 10:20:26 -0400

    eslint-config: warn console.log statements when committed
    We're seeing a decent amount of PR reviews comment on stray console.logs. We can
    just stop that at the source with an eslint configuration.
    
    Setting this to `warn` to not prevent or halt any builds upstream if DBConsole
    has any console.logs in the repo. IIRC we cleared it for cloud a bit ago.
    
    Release note: None

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -13,6 +13,7 @@ module.exports = {
     es6: true,
   },
   rules: {
+    "no-console": ["warn", { allow: ["warn", "error"] }],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
     "prettier/prettier": [
