6fa469878a0ced2486e79f5befbd1c12e6963c16
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2023-06-14 10:10:50 -0400

    ui-components, storybook: Updating Spinner props size, intent
    To account for a use of <Spinner /> in CockroachDB Cloud Console, I'm
    extending Spinner to include an "x-small" size and adding "intents"
    (color fills based on design system intent colors).
    
    I've also updated the Storybook story to display these prop changes,
    borrowing some of the structural components from the Icon stories.

--- packages/storybook-ui-components/stories/Spinner.stories.tsx
+++ packages/storybook-ui-components/stories/Spinner.stories.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { withKnobs, select } from "@storybook/addon-knobs";
-import { Spinner } from "@cockroachlabs/ui-components";
+import { Spinner, SpinnerSize, SpinnerFill } from "@cockroachlabs/ui-components";
 import { StoryContainer, StoryDescription  } from "./layout";
 
+import { IconDisplaySection, IconDisplay, IconLabel, IconFrame } from "./Icon.stories";
 export default {
   title: "Spinner",
   component: Spinner,
   decorators: [withKnobs],
 };
+const sizes: Array<{ key: SpinnerSize; size: number }> = [
+  { key: "x-small", size: 16 },
+  { key: "small", size: 24 },
+  { key: "default", size: 40 },
+  { key: "large", size: 64 },
+];
+const fills: Array<SpinnerFill> = [
+  "danger",
+  "default",
+  "info",
+  "primary",
+  "success",
+  "warning",
+  "inverted",
+];
+
 
 export const Example = () => (
   <StoryContainer>
@@ -17,20 +34,42 @@ export const Example = () => (
       render its content.
     </StoryDescription>
     <section>
-      <h2>Spinner sizes</h2>
-      <h4>Default size</h4>
-      <Spinner />
-      <h4>Small size</h4>
-      <Spinner size="small" />
-      <h4>Large size</h4>
-      <Spinner size="large" />
+      <h2>Spinner Sizes</h2>
+      <IconDisplaySection>
+        {sizes.map(s => (
+          <IconDisplay key={s.key}>
+            <IconLabel text={`${s.key} (${s.size}px)`} />
+            <IconFrame style={{ margin: "1rem auto" }}>
+              <Spinner size={`${s.key}`} />
+            </IconFrame>
+          </IconDisplay>
+        ))}
+      </IconDisplaySection>
     </section>
 
+    <section>
+      <h2>Spinner Fills</h2>
+      <IconDisplaySection>
+        {fills.map(fill => {
+          return (
+            <IconDisplay
+              key={fill}
+              backgroundColor={fill === "inverted" ? "#c0c6d9" : ""}
+            >
+              <IconLabel text={fill} />
+              <IconFrame style={{ alignItems: "center", justifyContent: "center" }}>
+                <Spinner fill={fill} />
+              </IconFrame>
+            </IconDisplay>
+          );
+        })}
+      </IconDisplaySection>
+    </section>
   </StoryContainer>
 );
 
 export const Demo = () => (
   <StoryContainer>
-    <Spinner size={select("size", ["default", "small", "large"], "default")} />
+    <Spinner size={select("size", ["default","x-small", "small", "large"], "default")} />
   </StoryContainer>
 )
--- packages/ui-components/src/Spinner/Spinner.tsx
+++ packages/ui-components/src/Spinner/Spinner.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import classNames from "classnames/bind";
 import styles from "./Spinner.module.scss";
 import objectToClassnames from "../utils/objectToClassnames";
+import { IconIntent } from "../Icon";
 
 const cx = classNames.bind(styles);
 
-export type SpinnerSize = "default" | "large" | "small";
+export type SpinnerSize = "default" | "large" | "small" | "x-small";
+
+export type SpinnerFill = IconIntent | "inverted";
 
 type OwnSpinnerProps = {
   size?: SpinnerSize;
+  fill?: SpinnerFill;
   className?: string;
 };
 
@@ -21,9 +25,14 @@ export type SpinnerProps = NativeSpinnerProps & OwnSpinnerProps;
 
 export const Spinner: React.FC<SpinnerProps> = ({
   size = "default",
+  fill = "default",
   className,
 }) => {
-  const classNames = cx("spinner", objectToClassnames({ size }), className);
+  const classNames = cx(
+    "spinner",
+    objectToClassnames({ size, fill }),
+    className,
+  );
   return (
     <svg
       className={classNames}
