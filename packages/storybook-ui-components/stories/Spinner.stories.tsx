f14f57b9b91be2536e22e2db31115ca8ccbc51cc
Nathan Stilwell <nathanstilwell@cockroachlabs.com>
2023-06-14 10:52:57 -0400

    storybook: playing with the storybook 7 format

--- packages/storybook-ui-components/stories/Spinner.stories.tsx
+++ packages/storybook-ui-components/stories/Spinner.stories.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { withKnobs, select } from "@storybook/addon-knobs";
+import type { Meta, StoryObj } from "@storybook/react";
 import { Spinner, SpinnerSize, SpinnerFill } from "@cockroachlabs/ui-components";
 import { StoryContainer, StoryDescription  } from "./layout";
 
 import { IconDisplaySection, IconDisplay, IconLabel, IconFrame } from "./Icon.stories";
-export default {
-  title: "Spinner",
-  component: Spinner,
-  decorators: [withKnobs],
-};
+
 const sizes: Array<{ key: SpinnerSize; size: number }> = [
   { key: "x-small", size: 16 },
   { key: "small", size: 24 },
@@ -25,51 +21,97 @@ const fills: Array<SpinnerFill> = [
   "inverted",
 ];
 
+const meta: Meta<typeof Spinner> = {
+  title: "Spinner",
+  component: Spinner,
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <StoryContainer>
+        <Story />
+      </StoryContainer>
+    ),
+  ],
+  argTypes: {
+    size: {
+      options: sizes.map(s => s.key),
+      control: "select",
+    },
+    fill: {
+      options: fills,
+      control: "select",
+    }
+  }
+};
 
-export const Example = () => (
-  <StoryContainer>
-    <h1>Spinner</h1>
-    <StoryDescription>
-      Loading indicator for components that might not receive all necessary data to
-      render its content.
-    </StoryDescription>
-    <section>
-      <h2>Spinner Sizes</h2>
-      <IconDisplaySection>
-        {sizes.map(s => (
-          <IconDisplay key={s.key}>
-            <IconLabel text={`${s.key} (${s.size}px)`} />
-            <IconFrame style={{ margin: "1rem auto" }}>
-              <Spinner size={`${s.key}`} />
-            </IconFrame>
-          </IconDisplay>
-        ))}
-      </IconDisplaySection>
-    </section>
+export default meta;
 
-    <section>
-      <h2>Spinner Fills</h2>
-      <IconDisplaySection>
-        {fills.map(fill => {
-          return (
-            <IconDisplay
-              key={fill}
-              backgroundColor={fill === "inverted" ? "#c0c6d9" : ""}
-            >
-              <IconLabel text={fill} />
-              <IconFrame style={{ alignItems: "center", justifyContent: "center" }}>
-                <Spinner fill={fill} />
+type Story = StoryObj<typeof Spinner>;
+
+export const Demo: Story = {
+  args: {
+    size: "default",
+    fill: "default",
+  },
+  tags: ["autodocs"],
+  render: (props) => {
+    return (
+      <StoryContainer>
+        <Spinner size={props.size} fill={props.fill} />
+      </StoryContainer>
+    );
+  },
+};
+
+export const Example: Story = {
+  argTypes: {
+    size: { control: false },
+    fill: { control: false },
+  },
+  parameters: {
+    controls: {
+      hideNoControlsWarning: true,
+    },
+  },
+  render: () => (
+    <StoryContainer>
+      <h1>Spinner</h1>
+      <StoryDescription>
+        Loading indicator for components that might not receive all necessary data to
+        render its content.
+      </StoryDescription>
+      <section>
+        <h2>Spinner Sizes</h2>
+        <IconDisplaySection>
+          {sizes.map(s => (
+            <IconDisplay key={s.key}>
+              <IconLabel text={`${s.key} (${s.size}px)`} />
+              <IconFrame style={{ margin: "1rem auto" }}>
+                <Spinner size={`${s.key}`} />
               </IconFrame>
             </IconDisplay>
-          );
-        })}
-      </IconDisplaySection>
-    </section>
-  </StoryContainer>
-);
+          ))}
+        </IconDisplaySection>
+      </section>
 
-export const Demo = () => (
-  <StoryContainer>
-    <Spinner size={select("size", ["default","x-small", "small", "large"], "default")} />
-  </StoryContainer>
-)
+      <section>
+        <h2>Spinner Fills</h2>
+        <IconDisplaySection>
+          {fills.map(fill => {
+            return (
+              <IconDisplay
+                key={fill}
+                backgroundColor={fill === "inverted" ? "#c0c6d9" : ""}
+              >
+                <IconLabel text={fill} />
+                <IconFrame style={{ alignItems: "center", justifyContent: "center" }}>
+                  <Spinner fill={fill} />
+                </IconFrame>
+              </IconDisplay>
+            );
+          })}
+        </IconDisplaySection>
+      </section>
+    </StoryContainer>
+  ),
+};
